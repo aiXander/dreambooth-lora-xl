66914ee4d13d90f0d7e75ba8470ff863c45b39e5
xander <xandersteenbruggex@gmail.com>
2023-10-31 11:30:08 -0700

    rename train_data_dir to instance_data_dir

--- train_dreambooth_lora_sdxl.py
+++ train_dreambooth_lora_sdxl.py
@@ -542,10 +542,10 @@ def parse_args(input_args=None):
     else:
         args = parser.parse_args()
 
-    if args.dataset_name is None and args.train_data_dir is None:
+    if args.dataset_name is None and args.instance_data_dir is None:
         raise ValueError("Specify either `--dataset_name` or `--instance_data_dir`")
 
-    if args.dataset_name is not None and args.train_data_dir is not None:
+    if args.dataset_name is not None and args.instance_data_dir is not None:
         raise ValueError("Specify only one of `--dataset_name` or `--instance_data_dir`")
 
     if args.train_text_encoder and args.train_text_encoder_ti:
